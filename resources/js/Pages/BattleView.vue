d6f09a6a9095c7bd945cdd0f9855d32ad71c81ed
dev-nass <jonasvincemaca@gmail.com>
2026-04-25 23:09:05 +0800

    add: word fetch

--- resources/js/Pages/BattleView.vue
+++ resources/js/Pages/BattleView.vue
@@ -1,3 +1,14 @@
-<script lang="ts"></script>
+<script setup lang="ts">
+import { useFetchWords } from "@/composables/battle/useFetchWords";
+import { onMounted } from "vue";
+
+const { words, fetchData } = useFetchWords();
+
+onMounted(async () => {
+    await fetchData();
+
+    console.log(words.value);
+});
+</script>
 
 <template>Battle View</template>
--- resources/js/composables/battle/useFetchWords.ts
+++ resources/js/composables/battle/useFetchWords.ts
@@ -5,20 +5,23 @@ export function useFetchWords() {
 
     const fetchData = async (): Promise<void> => {
         try {
-            const response = await fetch("");
+            const response = await fetch(
+                "https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt"
+            );
 
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
 
-            const data = await response.json();
-            words.value.add(data.word);
+            const data = await response.text();
+            data.split("\n").forEach((word) => words.value.add(word.trim()));
         } catch (error) {
             throw new Error(`Failed to fetch ${error}`);
         }
     };
 
     return {
+        words,
         fetchData,
     };
 }
