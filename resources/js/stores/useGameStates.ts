299d83912b0ae81f80d2f09fb47c3665e905ed3f
dev-nass <jonasvincemaca@gmail.com>
2026-05-27 23:55:33 +0800

    ref: comment docs

--- resources/js/stores/useGameStates.ts
+++ resources/js/stores/useGameStates.ts
@@ -5,7 +5,7 @@ const status = ref<string>("");
 const dictionary = ref(new Set<string>());
 const grid = ref<any[]>([]); // the grid of shuffled letters where users can pick
 const selected = ref<number[]>([]); // holds the ID currently selected letters
-const currentWord = ref<string[]>([]);
+const currentWord = ref<string[]>([]); // current word being spelled
 const wordUsed = ref(new Set<string>()); // words that are already used
 
 const score = ref<number>(0);
