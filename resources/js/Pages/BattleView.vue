e9ff3e020bfc91af60a2118ec7819e80da62f335
dev-nass <jonasvincemaca@gmail.com>
2026-05-29 22:09:10 +0800

    add: background image

--- resources/js/Pages/BattleView.vue
+++ resources/js/Pages/BattleView.vue
@@ -30,83 +30,89 @@ onMounted(async () => {
 </script>
 
 <template>
-    <div class="min-h-screen bg-[#0C0F1A] flex flex-col px-6 py-12">
-        <!-- Top Status -->
-        <div class="text-center mb-8">
-            <h1 class="text-white font-pixel text-2xl">
-                Current Status: {{ status }}
-            </h1>
-        </div>
+    <div class="min-h-screen bg-[#0C0F1A] flex flex-col">
+        <section
+            class="bg-[url('/public/images/dungeon-v2.png')] bg-center bg-no-repeat bg-[size:110%]"
+        >
+            <!-- Top Status -->
+            <div class="text-center mb-8">
+                <h1 class="text-white font-pixel text-2xl">
+                    Current Status: {{ status }}
+                </h1>
+            </div>
 
-        <!-- Middle Section -->
-        <div class="flex-1 flex items-center mb-8">
-            <div
-                class="w-full flex flex-col lg:flex-row lg:items-center lg:relative lg:justify-between"
-            >
-                <!-- Characters row (sm/md) | pushed to edges (lg) -->
-                <div class="flex justify-between lg:contents">
-                    <!-- Left Character -->
-                    <div
-                        :class="knightClass"
-                        class="lg:flex-1 lg:flex lg:justify-start lg:pl-8"
-                    >
+            <!-- Middle Section -->
+            <div class="flex-1 flex items-center mb-8">
+                <div
+                    class="w-full flex flex-col lg:flex-row lg:items-center lg:relative lg:justify-between"
+                >
+                    <!-- Characters row (sm/md) | pushed to edges (lg) -->
+                    <div class="flex justify-between lg:contents">
+                        <!-- Left Character -->
                         <div
-                            class="w-40 h-40 sm:w-52 sm:h-52 lg:w-75 lg:h-75 bg-[#0D1526] border-2 border-[#A855F7] flex items-center justify-center"
+                            :class="knightClass"
+                            class="lg:flex-1 lg:flex lg:justify-start lg:pl-8"
                         >
-                            <span class="text-[#A855F7] font-pixel text-sm">
+                            <div
+                                class="w-40 h-40 sm:w-52 sm:h-52 lg:w-75 lg:h-75 flex items-center justify-center"
+                            >
+                                <span class="text-[#A855F7] font-pixel text-sm">
+                                    <span
+                                        class="text-[#A855F7] font-pixel text-sm"
+                                    >
+                                        <img
+                                            src="/public/images/knight.png"
+                                            alt="dragon"
+                                        />
+                                    </span>
+                                </span>
+                            </div>
+                        </div>
+
+                        <img
+                            v-if="fireballVisible"
+                            :class="fireballClass"
+                            src="/public/images/fireball.png"
+                            alt="fireball"
+                            class="absolute right-[15%] top-1/2 -translate-y-1/2 h-50 pointer-events-none"
+                            style="--fireball-travel: -60vw"
+                        />
+
+                        <!-- Right Character -->
+                        <div
+                            :class="dragonClass"
+                            class="lg:flex-1 lg:flex lg:justify-end lg:pr-8"
+                        >
+                            <div
+                                class="w-40 h-40 sm:w-52 sm:h-52 lg:w-75 lg:h-75 flex items-center justify-center"
+                            >
                                 <span class="text-[#A855F7] font-pixel text-sm">
                                     <img
-                                        src="/public/images/knight.png"
+                                        src="/public/images/dragon.png"
                                         alt="dragon"
                                     />
                                 </span>
-                            </span>
+                            </div>
                         </div>
                     </div>
 
-                    <img
-                        v-if="fireballVisible"
-                        :class="fireballClass"
-                        src="/public/images/fireball.png"
-                        alt="fireball"
-                        class="absolute right-[15%] top-1/2 -translate-y-1/2 h-50 pointer-events-none"
-                        style="--fireball-travel: -60vw"
-                    />
-
-                    <!-- Right Character -->
+                    <!-- Current Word: below characters on sm/md, absolutely centered on lg -->
                     <div
-                        :class="dragonClass"
-                        class="lg:flex-1 lg:flex lg:justify-end lg:pr-8"
+                        class="flex justify-center mt-6 lg:mt-0 lg:absolute lg:left-1/2 lg:top-1/2 lg:-translate-x-1/2 lg:-translate-y-1/2"
                     >
-                        <div
-                            class="w-40 h-40 sm:w-52 sm:h-52 lg:w-75 lg:h-75 bg-[#0D1526] border-2 border-[#A855F7] flex items-center justify-center"
-                        >
-                            <span class="text-[#A855F7] font-pixel text-sm">
-                                <img
-                                    src="/public/images/dragon.png"
-                                    alt="dragon"
-                                />
-                            </span>
+                        <div class="grid grid-cols-6 gap-3">
+                            <GridSquare
+                                v-for="(c, index) in currentWord"
+                                :key="index"
+                                @click="removeLetter(index)"
+                            >
+                                {{ c }}
+                            </GridSquare>
                         </div>
-                    </div>
-                </div>
-
-                <!-- Current Word: below characters on sm/md, absolutely centered on lg -->
-                <div
-                    class="flex justify-center mt-6 lg:mt-0 lg:absolute lg:left-1/2 lg:top-1/2 lg:-translate-x-1/2 lg:-translate-y-1/2"
-                >
-                    <div class="grid grid-cols-6 gap-3">
-                        <GridSquare
-                            v-for="(c, index) in currentWord"
-                            :key="index"
-                            @click="removeLetter(index)"
-                        >
-                            {{ c }}
-                        </GridSquare>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
 
         <!-- Bottom Section: Grid + Buttons -->
         <div class="flex flex-col items-center gap-6">
