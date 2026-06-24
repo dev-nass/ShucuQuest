b47879f0bd53b5f1337091a73cfb49996e7c788e
dev-nass <jonasvincemaca@gmail.com>
2026-06-24 11:51:09 +0800

    fix: health potion going above threshold

--- resources/js/composables/potions/useHealing.ts
+++ resources/js/composables/potions/useHealing.ts
@@ -4,7 +4,13 @@ export function useHealing() {
     const { playerHealth } = useGameStates();
 
     const applyHealing = (): void => {
-        playerHealth.value += 3;
+        if (playerHealth.value >= 5) return;
+
+        // TODO: no hard coded MAX_PLAYERHEALTH  (5)
+        let healthDiff = Math.abs(playerHealth.value - 5);
+        if (healthDiff == 2) playerHealth.value += 2;
+        else if (healthDiff == 1) playerHealth.value += 1;
+        else playerHealth.value += 3;
     };
 
     return {
